fe4a2f75ea8356974c67078391e793d4d30d0e80
tokechan <fleatoke@gmail.com>
2025-03-30 00:45:25 +0900

    Add:CRUD API

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,5 +1,5 @@
 
-import { Request, Response } from 'express';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
 import { AppDataSource } from '../../ormconfig';
 import { Todo } from '../models/Todo';
 
@@ -29,3 +29,48 @@ export const createTodo = async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Failed to create todo'});
     }
 };
+
+export const updateTodo: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const { title, completed } = req.body;
+
+    try {
+        const todoRepo = AppDataSource.getRepository(Todo);
+        const todo = await todoRepo.findOneBy({ id: Number(id) });
+
+        if (!todo) {
+            return res.status(404).json({ error: 'Todo not found' });
+        }
+
+        if (title !== undefined) todo.title = title;
+        if (completed !== undefined) todo.completed = completed;
+
+        const updatedTodo = await todoRepo.save(todo);
+        res.json(updatedTodo);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to update todo' });
+    }
+};
+
+export const deleteTodo: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const  { id } = req.params;
+
+    try {
+        const todoRepo = AppDataSource.getRepository(Todo);
+        const todo = await todoRepo.findOneBy({ id: Number(id) });
+
+        if (!todo) {
+            return res.status(404).json({ error: 'Todo not found' });
+        }
+
+        await todoRepo.remove(todo);
+        res.status(204).send();
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to delete todo' });   
+    }
+};
+
+    
